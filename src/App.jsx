ad96c146d7838e6950f10a9103e232ad49b836b6
junayet <muhammadjunayetmaruf@gmail.com>
2024-07-29 11:53:46 +0300

    navbar almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AddressNavbar from './Shared/AddressNavbar/AddressNavbar'
+import Navbar from './Shared/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <AddressNavbar/>
+      <Navbar/>
     </>
   )
 }
