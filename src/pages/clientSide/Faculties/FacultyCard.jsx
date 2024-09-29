6b387126e938246df1c74ca7813f8fc1c629357b
junayet <muhammadjunayetmaruf@gmail.com>
2024-09-29 19:13:46 +0300

    team member card and section responsive has updated

--- src/pages/clientSide/Faculties/FacultyCard.jsx
+++ src/pages/clientSide/Faculties/FacultyCard.jsx
@@ -10,9 +10,9 @@ const FacultyCard = ({ faculty, slide }) => {
 
 
 
-        <div className="bg-primary/70 p-1.5 lg:w-[290px] rounded-lg text-black">
-            <div className=" relative bg-gray-100">
-            {/* <div className='w-full h-full bg-white absolute'></div>
+        <div className="bg-primary/70 p-1.5 w-[290px] rounded-lg text-black">
+            <div className=" relative bg-gray-100 min-h-full">
+                {/* <div className='w-full h-full bg-white absolute'></div>
             <div className='w-full h-full bg-text_color/50 absolute'></div> */}
                 <div className='relative'>
                     <div className='flex gap-1 justify-end p-5 pb-3 items-center'>
@@ -22,25 +22,25 @@ const FacultyCard = ({ faculty, slide }) => {
 
                     </div>
 
-                    <div className='lg:h-[120px] relative  flex flex-col justify-center items-center gap-5 '>
+                    <div className='h-[120px] relative  flex flex-col justify-center items-center gap-5 '>
                         <div className='w-full h-3.5 bg-primary/70'></div>
                         <div className='w-full h-3.5 bg-primary/70'></div>
                         <div className='w-full h-full absolute top-0 flex justify-center items-center '>
-                           <div className=' bg-primary/70 size-14 lg:size-28 rounded-full relative overflow-hidden'>
-                           <div className='absolute top-0 w-full h-full bg-white'></div>
-                            <img className='w-full h-full rounded-full object-cover relative p-1 bg-primary/70' src={image} alt="" /></div>
+                            <div className=' bg-primary/70 size-28 rounded-full relative overflow-hidden'>
+                                <div className='absolute top-0 w-full h-full bg-white'></div>
+                                <img className='w-full h-full rounded-full object-cover relative p-1 bg-primary/70' src={image} alt="" /></div>
                         </div>
                     </div>
+                    <div className='flex flex-col justify-center items-center'>
+                        <h2 className='text-base font-bold capitalize'>{name}</h2>
+                        <h2 className='text-sm'>{designation}</h2>
 
-                    <div className='flex flex-col justify-center items-center '>
-                        <h2 className='text-[10px] lg:text-base font-bold capitalize'>{name}</h2>
-                        <h2 className='text-[10px] lg:text-sm'>{designation}</h2>
-                        <h2 className='text-[10px] lg:text-sm pt-1'>{contact}</h2>
-                        <h2 className='text-[10px] lg:text-sm break-words'>{email}</h2>
+                        <h2 className='text-sm pt-1'>{contact}</h2>
+                        <h2 className='text-sm break-words'>{email}</h2>
                     </div>
-    
-                    <div className='pb-2 lg:pb-7'>
-                        <div className='bg-gradient-to-r from-primary to-primary/70 w-[50%] text-white lg:font-semibold lg:mt-5 mt-2 ml-auto py-1 text-[10px] lg:text-sm pl-5'>
+
+                    <div className='pb-7'>
+                        <div className='bg-gradient-to-r from-primary to-primary/70 w-[50%] text-white font-semibold mt-5 ml-auto py-1 text-sm pl-5'>
                             <p>Experience:</p>
                             <p>{job_experience} Years +</p>
                         </div>
